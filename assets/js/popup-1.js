1eed1bce90ce0fa2ea5333b93e8771c4d096785b
Bonnie Hill <bonnie.hill@hotmail.com>
2019-06-10 16:46:09 +1000

    popups finished

---
+++ assets/js/popup-1.js
@@ -0,0 +1,68 @@
+$( document ).ready(function() {
+
+
+// description popup    
+$('.l-plus1').click(function() {
+  $('.p-plus1').addClass('show');
+});
+
+$('.p-plus1').click(function() {
+  $('.p-plus1').removeClass('show');
+});
+
+// colophon popup
+$('.l-plus2').click(function() {
+  $('.p-plus2').addClass('show');
+});
+
+$('.p-plus2').click(function() {
+  $('.p-plus2').removeClass('show');
+});
+
+// twitter popup 1
+$('.l-plus3').click(function() {
+  $('.p-plus3').addClass('show');
+});
+
+$('.p-plus3').click(function() {
+  $('.p-plus3').removeClass('show');
+});
+
+// text popup 1
+$('.l-plus4').click(function() {
+  $('.p-plus4').addClass('show');
+});
+
+$('.p-plus4').click(function() {
+  $('.p-plus4').removeClass('show');
+});
+
+// twitter popup 2
+$('.l-plus5').click(function() {
+  $('.p-plus5').addClass('show');
+});
+
+$('.p-plus5').click(function() {
+  $('.p-plus5').removeClass('show');
+});
+
+// twitter popup 3
+$('.l-plus6').click(function() {
+  $('.p-plus6').addClass('show');
+});
+
+$('.p-plus6').click(function() {
+  $('.p-plus6').removeClass('show');
+});
+
+// text popup 2
+$('.l-plus7').click(function() {
+  $('.p-plus7').addClass('show');
+});
+
+$('.p-plus7').click(function() {
+  $('.p-plus7').removeClass('show');
+});
+
+
+});
